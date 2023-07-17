7d26fe251913f2daebf7f333d4ec56a6cf56b2c4
Crowdbotics <engineering@crowdbotics.com>
2023-07-17 11:58:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-bird-dev-102242",
+  url: "https://shiny-bird-dev-102242.botics.co",
+  api: "https://shiny-bird-dev-102242.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
